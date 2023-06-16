de47ac25cc6f093fd2120d1b5e31bf9eb82e88fa
YASH GUPTA <91718061+Yash084@users.noreply.github.com>
2023-06-16 11:35:51 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState, useEffect } from 'react';
+// import Header from './components/Header/Header';
+// import List from './components/List/List';
+// import { getPlacesData } from './api';
+// import Location from './components/Geocoding/Location';
 
-function App() {
+// const App = () => {
+//   const [places, setPlaces] = useState([]);
+//   const [coordinates, setCoordinates] = useState({});
+//   const [filteredPlaces, setFilteredPlaces] = useState([]);
+
+
+
+//   const [type, setType] = useState('Restaurants');
+//   const [rating, setRating] = useState('All');
+
+//   const [city, setCity] = useState('');
+
+//   const handleCitySubmit = (city) => {
+//     setCity(city);
+//   };
+
+
+//   useEffect(()=>{
+//       const filteredPlaces = places.filter((place)=> place.rating > rating);
+//       setFilteredPlaces(filteredPlaces);
+//   }, [rating]);
+
+
+//   useEffect(() => {
+//     console.log('Type:', type);
+//     getPlacesData(coordinates, type).then((data) => {
+//       console.log(data);
+//       if (data) {
+//         setPlaces(data);
+//         setFilteredPlaces([]);
+//       }
+//     });
+//   }, [type, coordinates]);
+  
+
+//   const handleLocationUpdate = (lat, lon) => {
+//     setCoordinates({ latitude: lat, longitude: lon });
+//     console.log("latitude is: ",lat);
+//     console.log("longitide is: ",lon);
+//   };
+
+//   return (
+//     <div>
+//       <Header onCitySubmit={handleCitySubmit} />
+//       <Location city={city} onLocationUpdate={handleLocationUpdate} />
+//       <List places={ filteredPlaces.length? filteredPlaces :  places} type={type} setType={setType} rating={rating} setRating={ setRating} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import { getPlacesData } from './api';
+import Location from './components/Geocoding/Location';
+
+const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [type, setType] = useState('Restaurants');
+  const [rating, setRating] = useState('All');
+  const [city, setCity] = useState('');
+
+  const handleCitySubmit = (city) => {
+    setCity(city);
+  };
+
+  useEffect(() => {
+    const filteredPlaces = places.filter((place) => {
+      if (rating === 'All') {
+        return true;
+      }
+      return place.rating > parseFloat(rating);
+    });
+    setFilteredPlaces(filteredPlaces);
+  }, [places, rating]);
+
+  useEffect(() => {
+    getPlacesData(coordinates, type).then((data) => {
+      if (data) {
+        setPlaces(data);
+        setFilteredPlaces([]);
+      }
+    });
+  }, [type, coordinates]);
+
+  const handleLocationUpdate = (lat, lon) => {
+    setCoordinates({ latitude: lat, longitude: lon });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header onCitySubmit={handleCitySubmit} />
+      <Location city={city} onLocationUpdate={handleLocationUpdate} />
+      <List
+        places={filteredPlaces.length ? filteredPlaces : places}
+        type={type}
+        setType={setType}
+        rating={rating}
+        setRating={setRating}
+      />
     </div>
   );
-}
+};
 
 export default App;
+
